88ad5b973894c7b6e0b30608f8d43411b147bdbc
uvsq21918311 <elkadi96hotmail.com>
2020-03-26 20:28:12 +0100

    Update QuitCommand

--- src/main/java/fr/uvsq/pglp_4_2/command/QuitCommand.java
+++ src/main/java/fr/uvsq/pglp_4_2/command/QuitCommand.java
@@ -1,12 +1,20 @@
 package fr.uvsq.pglp_4_2.command;
 
+import fr.uvsq.pglp_4_2.Switch;
 
 public class QuitCommand implements Command {
+	
+	
+	
+	private Switch interrupteur;
+	
+	public QuitCommand(Switch interrupteur) {
+		this.interrupteur = interrupteur;
+	}
 
 	@Override
 	public void apply() {
-		// TODO Auto-generated method stub
-		
+		interrupteur.turnOff();
 	}
 	
 
